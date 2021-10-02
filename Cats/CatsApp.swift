bf9c01795844a19b1556e861e2f3fe2ebe532927
Stefan Arentz <stefan@arentz.ca>
2021-10-02 08:45:36 -0400

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Stefan Arentz on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
